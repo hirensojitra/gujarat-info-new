34c00e590b144b4523d3e17a077520a6ef1e19ab
Hiren Sojitra <hiren3@amnex.com>
2024-10-07 19:53:50 +0530

    Update about-us-routing.module.ts

--- src/app/module/about-us/about-us-routing.module.ts
+++ src/app/module/about-us/about-us-routing.module.ts
@@ -6,7 +6,19 @@ const routes: Routes = [
   {
     path: '',
     component: AboutUsComponent,
-    data: { title: 'User Profile', breadcrumb: 'User Profile' }
+    data: {
+      title: 'Proposal for Web Application Poster Generation Service | Gujarat Info',
+      description: 'Discover our web application for generating election campaign posters, festival posts, and other promotional activities. Customize posters with photos, names, addresses, designations, and contact details for efficient and personalized promotional material.',
+      keywords: 'poster generation, campaign posters, election posters, festival posts, promotional activities, customization, Gujarat Info, web application',
+      robots: 'index, follow',
+      'og:image': 'https://vmi2070714.contaboserver.net/api/v1/img/uploads/NyoSS/7ZiAC?quality=30',
+      'og:image:alt': 'Gujarat Info Logo',
+      'og:image:type': 'image/svg+xml',
+      'og:image:width': '1200',
+      'og:image:height': '630',
+      'og:title': 'Web Application Poster Generation Service | Gujarat Info',
+      'og:description': 'Generate personalized posters for campaigns and festivals with Gujarat Info’s web application. Easily customize with your photos, names, and contact details.',
+    }
   }
 ];
 
