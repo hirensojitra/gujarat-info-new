5e45c28924bb7d3b8a6baf7e6519ca95baa1ad0b
Hiren Sojitra <hirensojitra@users.noreply.github.com>
2025-03-10 23:32:24 +0530

    Update

--- dist/gujarat-uvach/server/chunk-P2MFNMEK.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-var t={production:!0,MasterApi:"http://localhost:5000/api/v1",cookieConfig:{secure:!0,sameSite:"Strict",httpOnly:!0,path:"/"}};export{t as a};
---
+++ dist/gujarat-uvach/server/chunk-SRWCYMJY.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-URYMII3D.mjs";import"./chunk-QDDGMCDV.mjs";import"./chunk-RIQUXMK6.mjs";import"./chunk-JPPSYH5R.mjs";import"./chunk-X6RFGL5P.mjs";import"./chunk-CGEXYLQB.mjs";import"./chunk-ZOQGZUTB.mjs";import"./chunk-YAE7DDPL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as TalukaModule};
